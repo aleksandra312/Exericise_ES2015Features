859b9c59b9237970c0305e25a2bb25693caf0ab2
aleksandra <oleksandra.klevchuk@gmail.com>
2021-02-12 19:21:54 -0600

    added object enhancements exercise file

---
+++ obj_enhancements/ES2015-obj-enh.js
@@ -0,0 +1,39 @@
+//Same keys and values - Write an ES2015 Version
+
+function createInstructor(firstName, lastName) {
+	return {
+		firstName,
+		lastName
+	};
+}
+
+//Computed Property Names - Write an ES2015 Version
+
+let favoriteNumber = 42;
+
+const instructor = {
+	firstName: 'Colt',
+	[favoriteNumber]: 'That is my favorite!'
+};
+
+//Object Methods - Write an ES2015 Version
+
+const instructor = {
+	firstName: 'Colt',
+	sayHi() {
+		return 'Hi!';
+	},
+	sayBye() {
+		return this.firstName + ' says bye!';
+	}
+};
+
+//createAnimal function - Write an ES2015 Version
+function createAnimal(species, verb, noise) {
+	return {
+		species,
+		[verb]() {
+			return noise;
+		}
+	};
+}
